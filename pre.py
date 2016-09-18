632aa8206304f4a244887901c9ac05e4f5aad023
dhillonsh <dhillonsh@gmail.com>
2016-09-17 21:22:20 -0700

    Update pre.py

--- pre.py
+++ pre.py
@@ -44,7 +44,7 @@ def process(raw):
             entry['topic'] = ""
             entry['project'] = ""
             entry['week'] = content
-            entry['date'] = base.format("MM/DD/YYYY")
+            entry['date'] = "ab"
         elif field == 'topic' or field == 'project':
             entry[field] = content
 
